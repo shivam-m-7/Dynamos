05d588820a2125c3069e5a4f39706ce9e2443d4f
root <root@hp-Victus.localdomain>
2022-10-11 13:04:24 +0530

    dummy Data for Project

---
+++ dummyData.js
@@ -0,0 +1,28 @@
+breakpoints = 768px , 1024px
+cardSize = 238px,316px
+
+
+localStorageKeys=[
+    'productDescription',
+    'wishList'
+]
+
+const menProduct = {
+    productCode:number,
+    images : [string...],
+    category:['Tops','Dresses','T-shirts','Shirts','Jackets','Sweatshirts','Tracksuits','Jeans','Sportswear']
+    isMenOrWomen:'Men'/'Women',
+    productTitle:string,
+    strikedOffPrice:number,
+    price:number,
+    color:string,
+    size:string -> 'S'/'M'/'L',
+    isInWishlist:boolean,
+    productDetails:string,
+    brand:string,
+    brandDescription:string,
+    lookAfterMe:string,
+    aboutMe:string,
+    discount:number
+}
+
